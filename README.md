27ee2bdfaaf3ad665ca6a6de3425f1ed326e99c5
Sam Polgar <sampolgar@gmail.com>
2022-09-30 16:43:59 +1000

    It's a mess but working

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-read
+Start ngrok with the command `ngrok http 4000 --host-header=localhost:4000`.
--- appconnector.js
+++ appconnector.js
@@ -3,31 +3,12 @@ const host = 'localhost';
 const port = 4000;
 const fs = require('fs').promises;
 
-// const requestListener = function (req, res) {
-//     fs.readFile(__dirname + '/index.html')
-//         .then(contents => {
-//             res.setHeader("Content-Type", "text/html");
-//             res.writeHead(200);
-//             res.end(contents);
-//         })
-//         .catch(err => {
-//             res.writeHead(500);
-//             res.end(err);
-//             return;
-//         });
-// }
-
-
-// const server = http.createServer(requestListener);
-// server.listen(port, host, () => {
-//     console.log(`Server is running on http://${host}:${port}`);
-// });
-
 let indexFile;
 
 const requestListener = function (req, res) {
     res.setHeader("Content-Type", "text/html");
     res.writeHead(200);
+    // res.write(req.url)
     res.end(indexFile);
 }
 
