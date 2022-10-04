4209e07a1864cc1f6f16f97bd95996d075261634
Sam Polgar <sampolgar@gmail.com>
2022-10-04 17:34:44 +1100

    polishing for github

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Start ngrok with the command `ngrok http 4000 --host-header=localhost:4000`.
 1. popping open the Templafy UI in a browser at the base library level
 2. popping Templafy UI at a selected document
 3. popping Templafy UI at a selected document and pre-populating the document with the data from the page
+
--- appconnector.js
+++ appconnector.js
@@ -1,27 +1,26 @@
-const http = require('http');
-const host = 'localhost';
+const http = require("http");
+const host = "localhost";
 const port = 4000;
-const fs = require('fs').promises;
+const fs = require("fs").promises;
 
 let indexFile;
 
 const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "text/html");
-    res.writeHead(200);
-    // res.write(req.url)
-    res.end(indexFile);
-}
+  res.setHeader("Content-Type", "text/html");
+  res.writeHead(200);
+  res.end(indexFile);
+};
 
 const server = http.createServer(requestListener);
 
 fs.readFile(__dirname + "/index.html")
-    .then(contents => {
-        indexFile = contents;
-        server.listen(port, host, () => {
-            console.log(`Server is running on http://${host}:${port}`);
-        });
-    })
-    .catch(err => {
-        console.error(`Could not read index.html file: ${err}`);
-        process.exit(1);
+  .then((contents) => {
+    indexFile = contents;
+    server.listen(port, host, () => {
+      console.log(`Server is running on http://${host}:${port}`);
     });
+  })
+  .catch((err) => {
+    console.error(`Could not read index.html file: ${err}`);
+    process.exit(1);
+  });
